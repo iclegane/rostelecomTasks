131669da53e7917ee539491ae04bbf5f2d43acaf
Viktor <vvs-PathOfExile@yandex.ru>
2023-05-17 14:04:38 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+ 
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader", 'postcss-loader'],
+            },
+            {
+                test: /\.css?$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/public/index.html",
+            filename: "index.html",
+            favicon: './src/public/favicon.ico'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'src/assets'),
+        },
+        compress: false,
+        open: false,
+        hot: true,
+        port: 8080,
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+};
