5086b510b578e5965f98abbbfede6fd7c7573ece
Viktor <vvs-PathOfExile@yandex.ru>
2023-05-17 14:36:49 +0300

    included BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/components/App';
- 
+import { BrowserRouter } from 'react-router-dom';
+import App from '@/components/App'; 
+
 const domNode = document.getElementById('root') as Element;
 const root = ReactDOM.createRoot(domNode);
 
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </React.StrictMode>
 );
